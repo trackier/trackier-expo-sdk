250cdc9c99b839bab48730f1cede4290b3b6371d
Sanu gupta <sanugupta266@gmail.com>
2025-07-10 11:55:30 +0530

    update: version

--- android/src/main/java/com/trackierexposdk/TrackierExpoSdkModule.kt
+++ android/src/main/java/com/trackierexposdk/TrackierExpoSdkModule.kt
@@ -36,7 +36,7 @@ class TrackierExpoSdkModule(reactContext: ReactApplicationContext) :
       initializeMap.getString("environment") ?: ""
     )
     sdkConfig.setSDKType("react_native_sdk")
-    sdkConfig.setSDKVersion("1.6.60")
+    sdkConfig.setSDKVersion("1.6.73")
     sdkConfig.setAppSecret(
       initializeMap.getString("secretId") ?: "",
       initializeMap.getString("secretKey") ?: ""
