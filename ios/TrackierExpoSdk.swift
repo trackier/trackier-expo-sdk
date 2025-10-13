bf24224aaf50a35735d5b506ca39372347de37e7
Sanu gupta <sanugupta266@gmail.com>
2025-10-13 17:32:14 +0530

    update: version update

--- ios/TrackierExpoSdk.swift
+++ ios/TrackierExpoSdk.swift
@@ -40,7 +40,7 @@ class TrackierExpoSdk: RCTEventEmitter, DeepLinkListener {
 		let config = TrackierSDKConfig(appToken: appToken , env: environment)
 		config.setSDKType(sdkType: "react_native_sdk")
 		config.setAppSecret(secretId: dict["secretId"] as! String, secretKey: dict["secretKey"] as! String)
-		config.setSDKVersion(sdkVersion: "1.6.76")
+		config.setSDKVersion(sdkVersion: "1.6.77")
 		if (deeplinking != nil) {
 			config.setDeeplinkListerner(listener: self)
 		}
