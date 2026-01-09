75e06f6906dd078762c5cf07925293d847d540e9
Rajat Tiwari <rajattiwari32@gmail.com>
2026-01-09 12:48:37 +0530

    update: ApptroveSDK nomenclature

---
+++ app.plugin.js
@@ -0,0 +1,45 @@
+const { withAppBuildGradle, withDangerousMod } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
+
+function withAppTroveAndroid(config) {
+  return withAppBuildGradle(config, (config) => {
+    const dep = `implementation "com.google.android.gms:play-services-ads-identifier:18.0.1"`;
+
+    if (!config.modResults.contents.includes('play-services-ads-identifier')) {
+      config.modResults.contents = config.modResults.contents.replace(
+        /dependencies\s*{/,
+        `dependencies {\n    ${dep}`
+      );
+    }
+
+    return config;
+  });
+}
+
+function withAppTroveIOS(config) {
+  return withDangerousMod(config, [
+    'ios',
+    async (config) => {
+      const podfilePath = path.join(
+        config.modRequest.platformProjectRoot,
+        'Podfile'
+      );
+
+      let podfile = fs.readFileSync(podfilePath, 'utf-8');
+
+      if (!podfile.includes("pod 'apptrove-ios-sdk'")) {
+        podfile += `\npod 'apptrove-ios-sdk'\n`;
+        fs.writeFileSync(podfilePath, podfile);
+      }
+
+      return config;
+    },
+  ]);
+}
+
+module.exports = function withAppTrove(config) {
+  config = withAppTroveAndroid(config);
+  config = withAppTroveIOS(config);
+  return config;
+};
