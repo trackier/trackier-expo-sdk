bb7950a6f0e25562860fa7c8f263efb6403a1548
Satyam Jha <satyam@trackier.com>
2026-01-30 11:57:05 +0530

    Feat: Add meta tags in plugin side

--- app.plugin.js
+++ app.plugin.js
@@ -1,4 +1,4 @@
-const { withAppBuildGradle, withDangerousMod } = require('@expo/config-plugins');
+const { withAppBuildGradle, withDangerousMod, withAndroidManifest } = require('@expo/config-plugins');
 const fs = require('fs');
 const path = require('path');
 
@@ -11,6 +11,56 @@ function withTrackierAndroid(config) {
         /dependencies\s*{/,
         `dependencies {\n    ${dep}`
       );
+    }
+
+    return config;
+  });
+}
+
+function withTrackierAndroidQueries(config) {
+  return withAndroidManifest(config, async (config) => {
+    const androidManifest = config.modResults;
+    if (!androidManifest.manifest.queries) {
+      androidManifest.manifest.queries = [];
+    }
+
+    const packagesToAdd = [
+      'com.facebook.katana',
+      'com.facebook.lite',
+      'com.instagram.android',
+    ];
+
+    let existingPackages = [];
+    if (Array.isArray(androidManifest.manifest.queries)) {
+      androidManifest.manifest.queries.forEach(query => {
+        if (query.package && Array.isArray(query.package)) {
+          query.package.forEach(pkg => {
+            if (pkg.$ && pkg.$['android:name']) {
+              existingPackages.push(pkg.$['android:name']);
+            }
+          });
+        }
+      });
+    }
+
+    const newPackages = packagesToAdd.filter(p => !existingPackages.includes(p));
+
+    if (newPackages.length > 0) {
+      let queriesTag = androidManifest.manifest.queries.find(q => q.package);
+      if (!queriesTag) {
+        queriesTag = { package: [] };
+        androidManifest.manifest.queries.push(queriesTag);
+      }
+
+      if (!queriesTag.package) {
+        queriesTag.package = [];
+      }
+
+      newPackages.forEach(pkgName => {
+        queriesTag.package.push({
+          $: { 'android:name': pkgName }
+        });
+      });
     }
 
     return config;
@@ -40,6 +90,7 @@ function withTrackierAndroid(config) {
 
 module.exports = function withTrackier(config) {
   config = withTrackierAndroid(config);
+  config = withTrackierAndroidQueries(config);
   //config = withTrackierIOS(config);
   return config;
 };
